47b1baf9a110715b4453245f2abbbe5443b3bad6
talengu <talenapp@gmail.com>
2018-01-18 12:39:19 +0800

    create uuid and upload to http://www.getwox.com/plugin/

--- bs_test.py
+++ bs_test.py
@@ -11,3 +11,11 @@ start_time=time.time()
 time.sleep(2)
 end_time=time.time()
 print('%0.2f'%(end_time-start_time))
+
+import uuid
+
+name = "test_name"
+namespace = "test_namespace"
+
+print(uuid.uuid1())  # 带参的方法参见Python Doc
+# 0d5efc38-fc09-11e7-9d57-38d547a9452b
